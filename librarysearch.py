14735ed947d7b0e66ac38c534a3fe84bce62b4d4
pahad2000 <43926244+pahad2000@users.noreply.github.com>
2020-02-21 05:58:03 +1100

    Update librarysearch.py

--- librarysearch.py
+++ librarysearch.py
@@ -2,34 +2,33 @@
 from bookBooking import *
 # scores, bookFlag, library 
 score = 0
-delay = 1
+days = 1
 parameter = 0
 highest = 0
 i = 0
 # Initial calculation of scores for each library when no books have scanned
 for i in range(0, 1):
     for lib in library: 
-        delay *= lib['sign']
+        days = lib['sign']
         score = lib['ship']
-        score =/ delay
+        score =/ days
     if score > highest:
         highest = score
     score = 0
-    delay = 1
+    days = 1
 # Parameter can be optimised later on 
 parameter = 10
 while True:
     for lib in library: 
-        delay *= lib['sign']
+        days = lib['sign']
         for book in lib['book']:
             if bookFlag[book] == False:
                 score += 1
                 bookFlag[book] = True
-        delay *= lib['ship'] 
+        days *= lib['ship'] 
         score =/ delay
     if score > parameter:
         parameter = score
     score = 0
-    delay = 1
 
 

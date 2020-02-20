fbba832b03f56aac87f2b628cf3c45c6831b699d
kevin.liang2552@gmail.com <kevin.liang2552@gmail.com>
2020-02-21 08:11:11 +1100

    finished my one zzzz

--- qualifiers.py
+++ qualifiers.py
@@ -1,4 +1,6 @@
 import sys
+import operator
+import math
 
 def read_input():
 	input_file = str(sys.argv[1])
@@ -8,9 +10,9 @@ def read_input():
 	line = f.readline()
 
 	inputs = line.split()
-	no_books = inputs[0]
-	no_libs = inputs[1]
-	no_days = inputs[2]
+	no_books = int(inputs[0])
+	no_libs = int(inputs[1])
+	no_days = int(inputs[2])
 
 	line = f.readline()
 
@@ -21,36 +23,107 @@ def read_input():
 	i = 0
 
 	for line in f:
+		print(i)
+		print(line)
 		library[i] = {}
 		inputs = line.split()
-		library[i]['num_books'] = inputs[0]
-		library[i]['signup'] = inputs[1]
-		library[i]['ship'] = inputs[2]
+		library[i]['num_books'] = int(inputs[0])
+		library[i]['signup'] = int(inputs[1])
+		library[i]['ship'] = int(inputs[2])
 		line = f.readline()
 		library[i]['books'] = list(map(int, line.split()))
+		library[i]['shipped'] = []
 		i += 1
 
 	f.close()
 
 	return(no_books, no_libs, no_days, books_scores, library)
 
-def determine_high_score_lib(books_scores, library):
+def determine_high_score_lib(no_days, books_scores, library, signedup_libs):
 	
+	scores = {}
+
 	for lib in library:
 		total_score = 0
-		for books in library[lib]['books']:
-			total_score += books_scores[books]
-		print(total_score)
+		if lib not in signedup_libs:
+			for books in library[lib]['books']:
+				total_score += books_scores[books]
+			scores[lib] = total_score
+	max_score_key = max(scores.items(), key=operator.itemgetter(1))[0]
+	#print(max_score_key)
+	return(max_score_key, max(scores.values()))
+
+def output(signedup_libs, library):
+	output = ''
+	output += str(len(signedup_libs)) + '\n'
+	for lib in signedup_libs:
+		output += str(lib) + ' ' + str(len(library[lib]['shipped'])) + '\n'
+		output += ' '.join(list(map(str, library[lib]['shipped']))) + '\n'
+	
+	output_file = str(sys.argv[1])[:-3] + 'out' 
+	file = open(output_file, 'w')
+	file.write(output)
+	file.close()
+
 
 def main():
 
 	(no_books, no_libs, no_days, books_scores, library) = read_input()
-	print(no_books)
-	print(no_libs)
-	print(no_days)
-	print(books_scores)
-	print(library)
-	determine_high_score_lib(books_scores, library)
+
+	signedup_libs = []
+	scanned_books = []
+
+	signup = False
+	signup_period = 0
+
+	day_counter = 0
+
+	while day_counter <= no_days:
+		#print("it is currently day no." + str(day_counter))
+
+		if signup == False and len(signedup_libs) < no_libs:
+			(highest_score_lib, highest_score) = determine_high_score_lib(no_days, books_scores, library, signedup_libs)
+			#print("signing up lib no." + str(highest_score_lib))
+			signup = True
+			signup_period += 1
+
+		elif signup_period < library[highest_score_lib]['signup']:
+			signup_period += 1
+
+		elif signup_period == library[highest_score_lib]['signup']:
+			signedup_libs.append(highest_score_lib)
+			signup_period = 0
+			#print("finished signing up lib no." + str(highest_score_lib))
+			signup = False
+
+
+		for lib in signedup_libs:
+			to_ship = 0
+			i = 0
+
+			while to_ship < library[lib]['ship']:
+				
+				#print(scanned_books)
+				##print(library[lib]['books'][i])
+				#print(to_ship)
+				if i >= library[lib]['num_books']:
+					break
+
+				elif library[lib]['books'][i] not in scanned_books:
+					print("Scanning: " + str(library[lib]['books'][i]))
+					library[lib]['shipped'].append(library[lib]['books'][i])
+					scanned_books.append(library[lib]['books'][i])
+					#print(scanned_books)
+					to_ship += 1
+
+				i += 1	
+
+
+		day_counter += 1
+
+	
+	output(signedup_libs, library)
+
 
 
 if __name__ == "__main__":

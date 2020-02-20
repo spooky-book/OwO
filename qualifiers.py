50c4e4d8d9f6564b626e4dd743871f9e1d6457da
kevin.liang2552@gmail.com <kevin.liang2552@gmail.com>
2020-02-21 05:31:17 +1100

    implemented input as dictionary

--- qualifiers.py
+++ qualifiers.py
@@ -1,2 +1,48 @@
 import sys
 
+def read_input():
+	input_file = str(sys.argv[1])
+
+	f = open(input_file, "r")
+
+	line = f.readline()
+
+	inputs = line.split()
+	no_books = inputs[0]
+	no_libs = inputs[1]
+	no_days = inputs[2]
+
+	line = f.readline()
+
+	books_scores = line.split()
+
+	library = {}
+
+	i = 0
+
+	for line in f:
+		library[i] = {}
+		inputs = line.split()
+		library[i]['num_books'] = inputs[0]
+		library[i]['signup'] = inputs[1]
+		library[i]['ship'] = inputs[2]
+		line = f.readline()
+		library[i]['books'] = line.split()
+		i += 1
+
+
+	f.close()
+
+	print(no_books)
+	print(no_libs)
+	print(no_days)
+	print(books_scores)
+	print(library)
+
+def main():
+
+	read_input()
+
+
+if __name__ == "__main__":
+	main()
